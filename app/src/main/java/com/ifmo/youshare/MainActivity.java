d0610a7e9af3b3c0e32f7fc22ba64145f2c9168d
slavik <slavayus@gmail.com>
2018-02-16 00:15:35 +0300

    added google+ OAuth2

--- app/src/main/java/com/ifmo/youshare/MainActivity.java
+++ app/src/main/java/com/ifmo/youshare/MainActivity.java
@@ -1,29 +1,59 @@
 package com.ifmo.youshare;
 
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.android.volley.toolbox.ImageLoader;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.ifmo.youshare.util.EventData;
+import com.ifmo.youshare.util.NetworkSingleton;
+import com.ifmo.youshare.util.Utils;
+
+import java.util.Arrays;
+
+public class MainActivity extends Activity
+        implements NavigationView.OnNavigationItemSelectedListener, EventsListFragment.Callbacks {
+
+    public static final String APP_NAME = "YouShare";
+    public static final String ACCOUNT_KEY = "accountName";
+    private static final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
+    private static final int REQUEST_GMS_ERROR_DIALOG = 1;
+    private static final int REQUEST_ACCOUNT_PICKER = 2;
+
+    private ImageLoader mImageLoader;
+    private GoogleAccountCredential credential;
+    private String mChosenAccountName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,24 +62,59 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ensureLoader();
+
+        credential = GoogleAccountCredential.usingOAuth2(getApplicationContext(), Arrays.asList(Utils.SCOPES));
+        // set exponential backoff policy
+        credential.setBackOff(new ExponentialBackOff());
+
+        if (savedInstanceState != null) {
+            mChosenAccountName = savedInstanceState.getString(ACCOUNT_KEY);
+        } else {
+            loadAccount();
+        }
+
+        credential.setSelectedAccountName(mChosenAccountName);
+        System.out.println(mChosenAccountName);
+        if(mChosenAccountName==null){
+            chooseAccount();
+        }
+        System.out.println(mChosenAccountName);
+//        mEventsListFragment = (EventsListFragment) getFragmentManager()
+//                .findFragmentById(R.id.list_fragment);
+    }
+
+    private void ensureLoader() {
+        if (mImageLoader == null) {
+            // Get the ImageLoader through your singleton class.
+            mImageLoader = NetworkSingleton.getInstance(this).getImageLoader();
+        }
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
+    }
+
+    private void loadAccount() {
+        SharedPreferences sp = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        mChosenAccountName = sp.getString(ACCOUNT_KEY, null);
+        invalidateOptionsMenu();
     }
 
     @Override
@@ -76,7 +141,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -94,8 +159,94 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    @Override
+    public ImageLoader onGetImageLoader() {
+        ensureLoader();
+        return mImageLoader;
+    }
+
+    @Override
+    public void onEventSelected(EventData liveBroadcast) {
+//        startStreaming(liveBroadcast);
+    }
+
+    @Override
+    public void onConnected(String connectedAccountName) {
+//        loadData();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_GMS_ERROR_DIALOG:
+                break;
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode == Activity.RESULT_OK) {
+                    haveGooglePlayServices();
+                } else {
+                    checkGooglePlayServicesAvailable();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == Activity.RESULT_OK && data != null
+                        && data.getExtras() != null) {
+                    String accountName = data.getExtras().getString(
+                            AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        mChosenAccountName = accountName;
+                        credential.setSelectedAccountName(accountName);
+                        saveAccount();
+                    }
+                }
+                break;
+        }
+
+    }
+
+
+    private void haveGooglePlayServices() {
+        // check if there is already an account selected
+        if (credential.getSelectedAccountName() == null) {
+            // ask user to choose account
+            chooseAccount();
+        }
+    }
+
+    public void showGooglePlayServicesAvailabilityErrorDialog(final int connectionStatusCode) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                        connectionStatusCode, MainActivity.this,
+                        REQUEST_GOOGLE_PLAY_SERVICES);
+                dialog.show();
+            }
+        });
+    }
+
+    private void checkGooglePlayServicesAvailable() {
+        final int connectionStatusCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+
+    private void chooseAccount() {
+        startActivityForResult(credential.newChooseAccountIntent(),
+                REQUEST_ACCOUNT_PICKER);
+    }
+
+    private void saveAccount() {
+        SharedPreferences sp = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        sp.edit().putString(ACCOUNT_KEY, mChosenAccountName).apply();
     }
 }
